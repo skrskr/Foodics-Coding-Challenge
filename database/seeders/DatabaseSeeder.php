5d1d931ebdba85816a4b7583299c26f0c8faebe6
Mohamed Sakr <mohamed1.sakr1996@gmail.com>
2023-10-02 23:52:53 +0300

    added product model with migrations and seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,8 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
         $this->call([
-            MeasurementUnitSeeder::class
+            // MeasurementUnitSeeder::class,
+            ProductSeeder::class,
         ]);
     }
 }
