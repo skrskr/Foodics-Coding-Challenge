7df4112c393c1e719092a198446e60253b5ef935
Mohamed Sakr <mohamed1.sakr1996@gmail.com>
2023-10-03 00:29:42 +0300

    added order product model with migration

---
+++ database/migrations/2023_10_02_212253_create_order_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order_products', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("product_id")->nullable();
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+
+            $table->unsignedBigInteger("order_id")->nullable();
+            $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
+            $table->decimal("price")->default(0);
+            $table->unsignedInteger("quantity");
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order_products');
+    }
+};
