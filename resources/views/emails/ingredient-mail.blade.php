5c227667bc24607e84bfaefcb79b83e2196c8f2c
Mohamed Sakr <mohamed1.sakr1996@gmail.com>
2023-10-04 01:04:34 +0300

    added ingredient quantity alert mail

---
+++ resources/views/emails/ingredient-mail.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Ingredient Quantity Alert!</title>
+</head>
+<body>
+    <h1>Your Ingredient {{$data['name']}} quantity percentage is {{$data['percentage']}}%</h1>
+
+    <p>Please make sure to buy sufficent quantity from this ingredient.</p>
+
+    <p>Best regards,<br>Support Team</p>
+</body>
+</html>
